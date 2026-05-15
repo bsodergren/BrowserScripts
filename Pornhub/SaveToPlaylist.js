72bf591204ebb535bbbf0297307f5575d6aa2740
bsodergren <bjorn.sodergren@gmail.com>
2026-05-15 18:05:11 -0500

    update

--- Pornhub/SaveToPlaylist.js
+++ Pornhub/SaveToPlaylist.js
@@ -1,30 +1,28 @@
 // ==UserScript==
-// @name        New script
-// @namespace   Violentmonkey Scripts
+// @name       Pornhub - Save to local playlist
+// @namespace https://greasyfork.org/users/984905
 // @match        https://*.pornhub.com/*
 // @match        https://*.pornhub.org/*
 // @match        https://*.pornhubpremium.com/*
 // @match        https://*.pornhubpremium.org/*
 // @grant       GM_xmlhttpRequest
-// @version     1.0
-// @author      -
+// @version     1.2
+// @author      Bjorn
 // @run-at       document-end
 // @description 4/28/2026, 6:34:22 AM
-// @require https://update.sleazyfork.org/scripts/578336/1826440/Bs%20ToastPopup.js
+// @require https://update.greasyfork.org/scripts/578336/1826440/Bs%20ToastPopup.js
 // ==/UserScript==
 
 
-(function () {
+; (function () {
     'use strict';
+    showToast("loaded",3000);
 
-let  d = document.getElementsByClassName('row-5-thumbs');
-  if (typeof d[0] !== "undefined") {
-
-  console.log(d[0]);
-   d[0].setAttribute("style",'    grid-template-columns: repeat(2, minmax(0, 1fr));')
-
-}
-
+    let d = document.getElementsByClassName('row-5-thumbs');
+    if (typeof d[0] !== "undefined") {
+        console.log(d[0]);
+        d[0].setAttribute("style", '    grid-template-columns: repeat(2, minmax(0, 1fr));')
+    }
 
     let savedLinks = [];
     document.addEventListener('contextmenu', function (e) {
@@ -46,9 +44,7 @@ let  d = document.getElementsByClassName('row-5-thumbs');
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
-
         });
-
-          showToast("Saved to playlist", 3000)
+        showToast("Saved to playlist", 3000)
     }
 }());
